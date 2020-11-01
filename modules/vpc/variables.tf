765a375cb3c60a0278c2ee0d55e44e531bc66dec
DanielWs <danielaaronw@gmail.com>
2020-11-01 09:58:51 -0800

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -8,26 +8,6 @@ variable "environment" {
   default = "Terraform_Project"
 }
 
-variable "pubsubnet1cidr" {
-  description = "The VPC cidr block"
-  default = "10.0.1.0/24"
-}
-
-variable "pubsubnet2cidr" {
-  description = "The VPC cidr block"
-  default = "10.0.2.0/24"
-}
-
-variable "privsubnet1cidr" {
-  description = "The VPC cidr block"
-  default = "10.0.3.0/24"
-}
-
-variable "privsubnet2cidr" {
-  description = "The VPC cidr block"
-  default = "10.0.4.0/24"
-}
-
 variable "pubsubnetcidrs" {
   type = list(string)
   description = "cidr blocks for public subnets"
